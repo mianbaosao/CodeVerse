ccdf4836a18a5262f8dc5d751fd91ac694aa6fdb
mianbao@example.com <1623666966@qq.com>
2024-10-24 20:37:34 +0800

    feat:完成cf算法的基础框架

--- mb-study-forum/mb-study-forum-domain/src/main/java/com/mianbao/forum/common/handler/recommend/CFAlgorithm.java
+++ mb-study-forum/mb-study-forum-domain/src/main/java/com/mianbao/forum/common/handler/recommend/CFAlgorithm.java
@@ -4,6 +4,9 @@ import com.mianbao.forum.common.entity.BlogDataBO;
 import com.study.forum.common.enums.RecommendAlgorithm;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * @Description: 协同过滤推荐算法
  * @Author:bread
@@ -18,7 +21,26 @@ public class CFAlgorithm implements RecommendType{
 
     @Override
     public BlogDataBO query(int userId) {
-        System.out.println("CB成功");
+        // 获取用户的点赞和收藏记录
+
+
+        // 随机选取 20 个其他用户
+
+
+        // 获取其他用户的点赞和收藏记录
+
+
+        // 计算 Jaccard 相似系数
+
+
+        // 获取相似用户点赞/收藏的内容，并过滤掉当前用户已操作的内容
+
+
+        // 如果推荐的内容不够，继续查找下一个相似用户
+
+
+        // 返回推荐结果，转换为 BlogDataBO
+
         return null;
     }
 }
